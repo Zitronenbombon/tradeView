befa9a8ba4c6c87ff7f57fda2adbc1627e5065fa
Zitronenbombon <lg068275@stud.leuphana.de>
2021-04-22 03:23:51 +0200

    Sprache Ändern hinzugefügt

--- app/src/main/java/com/example/tradeview/MainActivity.java
+++ app/src/main/java/com/example/tradeview/MainActivity.java
@@ -1,19 +1,83 @@
 package com.example.tradeview;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadLocale();
         setContentView(R.layout.activity_main);
+
+        Button changeLang = findViewById(R.id.changeLanguage);
+        changeLang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // show AlertDialog to display list of languages, one can be selected
+                showChangeLanguageDialog();
+                
+            }
+        });
+    }
+
+    private void showChangeLanguageDialog() {
+        //array of languages to choose from
+        final String[] listItems = {"English", "Deutsch"};
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+        mBuilder.setTitle("Choose Language");
+        mBuilder.setSingleChoiceItems(listItems, -1, new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i){
+                if (i == 0){
+                    setLocale("en");
+                    recreate();
+                }
+                if (i == 1){
+                    setLocale("de");
+                    recreate();
+                }
+
+                dialogInterface.dismiss();
+                }
+        });
+
+        AlertDialog mDialog = mBuilder.create();
+        //show alert dialog
+        mDialog.show();
+    }
+
+    private void setLocale (String lang){
+        Locale locale = new Locale(lang);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        //save data to shared preferences
+        SharedPreferences.Editor editor = getSharedPreferences("Settings", MODE_PRIVATE).edit();
+        editor.putString("My_Lang", lang);
+        editor.apply();
+    }
+
+    // load language saved in shared preferences
+    public void loadLocale(){
+        SharedPreferences prefs = getSharedPreferences("Settings", Activity.MODE_PRIVATE);
+        String language = prefs.getString("My_Lang", "");
+        setLocale(language);
     }
 
     public void logout (View view){
